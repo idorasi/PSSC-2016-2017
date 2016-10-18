61dfb9d809fd5dd5dfa6c93b905e10430b7b5e29
idorasi <idorasi.florin@gmail.com>
2016-10-18 21:56:07 +0300

    Added 4 new files.
    Added 4 classes.

---
+++ PSSC/Models/Decanat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+  
+    public class Decanat
+    {
+       
+        public void DefinesteDiscipline()
+        {
+
+        }
+        public void AlocaDiscipline()
+        {
+        }
+        public float CalculeazaMedieDisciplina()
+        {
+            return 0;
+        }
+        public void GenereazaRaport()
+        {
+
+        }
+    }
+}
